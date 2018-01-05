d0963a2e65a044c4618d40e97e59c7c4d139a481
Francesco Marrara <francmarrara@linuxmail.org>
2018-01-05 16:46:17 +0100

    testDB Updated

--- src/test/testDB.java
+++ src/test/testDB.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.List;
+
+import model.Prodotto;
 import persistenceDAO.DataBaseManager;
 
 public class testDB {
@@ -7,19 +10,25 @@ public class testDB {
 	public static void main(String args[]) {
 
 		DataBaseManager dbManager = new DataBaseManager();
-		dbManager.dropDataBase();
-		dbManager.createDataBase();
-		dbManager.createVenditori();
-		dbManager.createArmadi();
-		dbManager.createCassettiere();
-		dbManager.createComodini();
-		dbManager.createCredenze();
-		dbManager.createIlluminazioni();
-		dbManager.createLetti();
-		dbManager.createTavoli();
-		dbManager.createCucine();
-		dbManager.createLibrerie();
-		dbManager.createScrivanie();
+//		dbManager.dropDataBase();
+//		dbManager.createDataBase();
+//		dbManager.createVenditori();
+//		dbManager.createArmadi();
+//		dbManager.createCassettiere();
+//		dbManager.createComodini();
+//		dbManager.createCredenze();
+//		dbManager.createIlluminazioni();
+//		dbManager.createLetti();
+//		dbManager.createTavoli();
+//		dbManager.createCucine();
+//		dbManager.createLibrerie();
+//		dbManager.createScrivanie();
+
+List <Prodotto> prodotti = dbManager.getProdottoDao().findByTipo("Scrivania");
+
+for(Prodotto p : prodotti) {
+	p.stampaProdotto();
+}
 
 	}
 
