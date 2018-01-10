20ef54d0a9cf34d41f12763007d58e97a03a5702
Simone Crisafi <xblack90@hotmail.it>
2018-01-10 13:04:44 +0100

    modifiche home

--- WebContent/js/Js/index.js
+++ WebContent/js/Js/index.js
@@ -1,5 +1,29 @@
-/*Allinea il menu all'header quando si scrolla la pagina */
+/*Allinea il menu all'header quando si scrolla la pagina con scomparsa */
 $(document).ready(function() {
     /* affix the navbar after scroll below header */
     $(".navbar").affix({ offset: { top: $("header").outerHeight(true) } });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() < 100) {
+            $('.navbar').fadeIn(400);
+        } else if ($(this).scrollTop() > 150) {
+            $('.navbar').fadeIn(550);
+        } else {
+            $('.navbar').fadeOut(400);
+        }
+
+    })
+});
+
+// Pannello dei prodotti in offerta
+$(document).ready(function() {
+    window.onload = function() {
+        $('.slider').slick({
+            autoplay: true,
+            autoplaySpeed: 1500,
+            centerMode: true,
+            slidesToShow: 3,
+            slidesToScroll: 1
+        });
+    };
 });
