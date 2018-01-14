712164e71db9dce9169ff2459f64e06dc27a1d6c
Simone Crisafi <xblack90@hotmail.it>
2018-01-14 13:17:54 +0100

    modifiche accountPage 2.0

--- WebContent/js/Js/profiloUtente.js
+++ WebContent/js/Js/profiloUtente.js
@@ -1,3 +1,29 @@
 /**
  * 
  */
+
+/*Allinea il menu all'header quando si scrolla la pagina con scomparsa */
+$(document).ready(function() {
+    /* affix the navbar after scroll below header */
+    $(".navbar").affix({ offset: { top: $("header").outerHeight(true) } });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() < 100) {
+            $('.navbar').fadeIn(400);
+        } else if ($(this).scrollTop() > 150) {
+            $('.navbar').fadeIn(550);
+        } else {
+            $('.navbar').fadeOut(400);
+        }
+
+    })
+});
+
+// Menu Catalogo Prodotti - Mostra/Nascondi al passaggio del mouse
+$(document).ready(function() {
+    $("#catalogoProdotti").hover(function() {
+        $(this).find("#menuProdotti").show(250);
+    }, function() {
+        $(this).find("#menuProdotti").hide(250);
+    });
+});
