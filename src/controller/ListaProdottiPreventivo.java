e4f79625714153ad3fe5365b28127bc063a48106
Simone Crisafi <xblack90@hotmail.it>
2018-01-29 17:28:50 +0100

    listaProdotti 2.0

--- src/controller/ListaProdottiPreventivo.java
+++ src/controller/ListaProdottiPreventivo.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CreaListaProdottiPerPreventivo extends HttpServlet{
+public class ListaProdottiPreventivo extends HttpServlet{
 
 	/**
 	 * 
