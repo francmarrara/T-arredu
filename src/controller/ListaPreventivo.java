aa46395a40211e10d72edb4b4b3f6831b49493e6
Simone Crisafi <xblack90@hotmail.it>
2018-01-30 12:21:12 +0100

    merge SimonBranch 2.0

--- src/controller/ListaPreventivo.java
+++ src/controller/ListaPreventivo.java
@@ -2,10 +2,6 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-<<<<<<< HEAD
-import java.util.ArrayList;
-=======
->>>>>>> master
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,52 +19,35 @@ public class ListaPreventivo extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String[] prodotti = req.getParameterValues("prodotti[]");
-<<<<<<< HEAD
-//		String[] richieste = req.getParameterValues("richieste[]");
 
-		
-		
-=======
+		// ArrayList<ProdottoConImmagini> prodottiDaInserireNelPreventivo = new
+		// ArrayList<ProdottoConImmagini>();
+		//
+		// DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+		// ProdottoDAO prodottoDao = factory.getProdottoDAO();
 
->>>>>>> master
-//		ArrayList<ProdottoConImmagini> prodottiDaInserireNelPreventivo = new ArrayList<ProdottoConImmagini>();
-//
-//		DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
-//		ProdottoDAO prodottoDao = factory.getProdottoDAO();
-<<<<<<< HEAD
-		
 		for (int i = 0; i < prodotti.length; i++) {
 			System.out.println(prodotti[i]);
-//			ProdottoConImmagini p = prodottoDao.findByPrimaryKeyProdottoConImmagini(Integer.parseInt(prodotti[i]));
-//			p.setRichiestaAggiuntivaInPreventivo(richieste[i]);
+			// ProdottoConImmagini p =
+			// prodottoDao.findByPrimaryKeyProdottoConImmagini(Integer.parseInt(prodotti[i]));
+			// p.setRichiestaAggiuntivaInPreventivo(richieste[i]);
 
-//			prodottiDaInserireNelPreventivo.add(p);
+			// prodottiDaInserireNelPreventivo.add(p);
 
-=======
-//
-		for (int i = 0; i < prodotti.length; i++) {
+			//
+			// for (int i = 0; i < prodotti.length; i++) {
 
-		//	ProdottoConImmagini p = prodottoDao.findByPrimaryKeyProdottoConImmagini(Integer.parseInt(prodotti[i]));
+			// ProdottoConImmagini p =
+			// prodottoDao.findByPrimaryKeyProdottoConImmagini(Integer.parseInt(prodotti[i]));
 
-System.out.println(prodotti[i]);
->>>>>>> master
+			System.out.println(prodotti[i]);
 		}
-//
-//		for (ProdottoConImmagini p : prodottiDaInserireNelPreventivo) {
-//
-//		}
-<<<<<<< HEAD
-		
-		PrintWriter out = resp.getWriter();
-		out.println(prodotti[0]);
-		
-		
-=======
+		//
+		// for (ProdottoConImmagini p : prodottiDaInserireNelPreventivo) {
+		//
+		// }
 
->>>>>>> master
 
-		
-		
 	}
 
 }
