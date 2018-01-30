5be94136c290bf4e1c0abcf93d49e456b931a4ef
Francesco Marrara <francmarrara@linuxmail.org>
2018-01-30 18:49:14 +0100

    email 1.0

--- src/controller/ListaPreventivo.java
+++ src/controller/ListaPreventivo.java
@@ -1,24 +1,31 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.ProdottoConImmagini;
-import model.Venditore;
 import persistenceDAO.DAOFactory;
 import persistenceDAO.ProdottoDAO;
 
 public class ListaPreventivo extends HttpServlet {
-
+	private String emailUtente;
 	/**
 	 * 
 	 */
@@ -27,6 +34,9 @@ public class ListaPreventivo extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		setEmailUtente((String) req.getSession().getAttribute("emailUtenteLoggato"));
+		
+		
 		String[] prodotti = req.getParameterValues("prodotti[]");
 		String[] richieste = req.getParameterValues("richieste[]");
 
@@ -58,16 +68,61 @@ public class ListaPreventivo extends HttpServlet {
 
 		}
 
-		System.out.println(venditoriEProdotti.toString());
+		// System.out.println(venditoriEProdotti.toString());
 
 		for (ProdottoConImmagini prodotto : prodottiDaInserireNelPreventivo) {
 
 			venditoriEProdotti.get(prodotto.getEmailVenditore()).add(prodotto);
 
 		}
-		
-		System.out.println(venditoriEProdotti.toString());
 
+		// System.out.println(venditoriEProdotti.toString());
+		HttpSession session = req.getSession();
+		//sendEmail();
+		
+	}
+
+	public void sendEmail(String destinatario, String oggetto, String messaggio) {
+		final String username = "tarredu.arredamenti@gmail.com";
+		final String password = "Cacazza01!";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("tarredu.arredamenti@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(destinatario));
+			message.setSubject(oggetto);
+			message.setText(messaggio);
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getEmailUtente() {
+		return emailUtente;
+	}
+
+	public void setEmailUtente(String emailUtente) {
+		this.emailUtente = emailUtente;
 	}
 
 }
